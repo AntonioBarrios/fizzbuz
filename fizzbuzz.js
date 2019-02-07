cccb9acd635173a8e194667bae95c65dbb261dbc
Antonio Arango <31933285+AntonioBarrios@users.noreply.github.com>
2019-02-07 17:06:46 -0600

    Create fizzbuzz.js

---
+++ fizzbuzz.js
@@ -0,0 +1,12 @@
+//Resolviendo FizzBuz
+
+for(var i=1; i<=100; i++){
+  let resultado= '';
+  if (i % 3 == 0){
+    resultado+= 'Fizz';
+  }
+  if( i % 5 == 0){
+    resultado+= 'Buzz';
+  }
+  console.log(resultado || i);
+}
